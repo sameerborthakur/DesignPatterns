139ce860f253ab0f9199518c7061a46fa4e1918c
Sameer Borthakur <borthakur.s@extra.com>
2026-03-12 15:33:08 +0530

    design pattern

---
+++ src/main/java/patterns/creational/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package patterns.creational.singleton;
+
+public class Singleton
+{
+	public static Singleton instance;
+
+	private Singleton(){
+
+	}
+
+	public static Singleton getInstance(){
+		if(instance == null){
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
